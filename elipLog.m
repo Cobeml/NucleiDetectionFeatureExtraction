35d3e52660f0118d4157b49af8b427dc499163c0
Cobe Liu <liucobe@gmail.com>
2023-09-22 11:21:18 -0400

    Update elipLog.m

--- elipLog.m
+++ elipLog.m
@@ -1,7 +1,7 @@
 function [h2] = elipLog(p2, sigma_x, sigma_y, theta)
 
 % returns a rotational
-%   Laplacian of Gaussian filter of size HSIZE with standard deviation
+%   Determinant of Hessian of Gaussian filter of size HSIZE with standard deviation
 %   SIGMA (positive). HSIZE can be a vector specifying the number of rows
 %   and columns in H or a scalar, in which case H is a square matrix.
 %   The default HSIZE is [5 5], the default SIGMA is 0.5.
@@ -10,7 +10,7 @@ function [h2] = elipLog(p2, sigma_x, sigma_y, theta)
 % sigma_x = 4;
 % sigma_y = 4;
 %theta = 
-% Laplacian of Gaussian
+% Determinant of Hessian of Gaussian
 % first calculate Gaussian
 siz   = (p2-1)/2;
 
@@ -31,8 +31,8 @@ sumh = sum(h(:));
 if sumh ~= 0,
     h  = h/sumh;
 end;
-% now calculate Laplacian
+% now calculate determinant of hessian
 %h1 = h.*(x.*x + y.*y - 2*std2)/(std2^2);
-lapl = ((2*a*x+2*b*y).^2-2*a)+((2*c*y+2*b*x).^2-2*c);
-h1 = h.*lapl;
+hess = ((2*a*x+2*b*y).^2-2*a).*((2*c*y+2*b*x).^2-2*c) - ((2*a*x+2*b*y).*(2*b*x+2*c*y)-2*b).^2;
+h1 = h.*h.*hess;
 h2 = h1 - sum(h1(:))/prod(p2); % make the filter sum to zero
